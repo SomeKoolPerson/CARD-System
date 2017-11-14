10233a0e11a7a6fcddbe067129c74233e9979834
Shadiekid <Njtulio8@gmail.com>
2017-11-14 16:50:21 -0500

    Add files via upload

--- test/test/Form1.Designer.cs
+++ test/test/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(284, 312);
+            this.button1.Location = new System.Drawing.Point(189, 203);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(157, 41);
+            this.button1.Size = new System.Drawing.Size(105, 27);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,24 +46,26 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
             this.listBox1.Items.AddRange(new object[] {
             " "});
-            this.listBox1.Location = new System.Drawing.Point(29, 29);
+            this.listBox1.Location = new System.Drawing.Point(19, 19);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(1270, 264);
+            this.listBox1.Size = new System.Drawing.Size(848, 173);
             this.listBox1.TabIndex = 1;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1341, 365);
+            this.ClientSize = new System.Drawing.Size(894, 237);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- test/test/Form1.cs
+++ test/test/Form1.cs
@@ -43,32 +43,38 @@ namespace test
             List<string> links = new List<string>();
             List<string> types = new List<string>();
             WebClient web = new WebClient();
-            string cardhtml = web.DownloadString("https://deckbox.org/games/mtg/cards");
-            MatchCollection m1 = Regex.Matches(cardhtml, @"<a class='simple' href=(.+?)</tr>", RegexOptions.Singleline);
+            string cardhtml = web.DownloadString("https://deckbox.org/games/mtg/cards?f=g5MC4wMQ**");
+            MatchCollection m1 = Regex.Matches(cardhtml, @"<td class='card_name'>(.+?)r>", RegexOptions.Singleline);
 
-            for (int i = 1; i < m1.Count - 1; i++)
+            for (int i = 0; i < m1.Count; i++)
             {
                 string card = m1[i].Groups[1].Value;
                 cards.Add(card);
             }
 
+            Console.WriteLine(cards[0]);
 
             for (int i = 0; i < cards.Count; i++)
             {
-                //string name = Parse(cards[i], "target='_blank'>", "</a>");
-                //names.Add(name);
-                Console.WriteLine(cards[i]);
-                //string price = Parse(cards[i], "<td class='center minimum_width'>", "</td>");
-                //prices.Add(price);
-                //string type = Parse(cards[i], "<td>", "</td><td class='center'>");
-                //types.Add(type);
-                //string link = Parse(cards[i], "href='", "'");
-                //links.Add(link);
-                //cards[i] = name + " - "+ price;
+                string link = Parse(cards[i], "<a class='simple' href='", "' target='_blank'>");
+                links.Add(link);
+                string name = Parse(cards[i], "target='_blank'>", "</a>");
+                names.Add(name);
+                string price = Parse(cards[i], "<td class='center minimum_width'>", "</td>\n</t");
+                prices.Add(price);
+                string type = Parse(cards[i], "</a>\n</td>\n<td>", "</td>\n<td class='center'>");
+                types.Add(type);
+
+                cards[i] = name + " - " + type + " - " + price + " - " + link;
 
             }
 
             listBox1.DataSource = cards;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
