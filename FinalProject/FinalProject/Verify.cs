fa45bd268393beddbd5204ff7c7a27ffc55f377d
SpacePot8o <33265763+SpacePot8o@users.noreply.github.com>
2017-11-22 13:37:59 -0500

    Add files via upload

--- FinalProject/FinalProject/Verify.cs
+++ FinalProject/FinalProject/Verify.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.Net;
+using System.Security.Cryptography;
 
 namespace FinalProject
 {
@@ -18,7 +22,7 @@ namespace FinalProject
         private Login _login;
         private String _connectionString;
 
-        private String _code = "666";
+        private static String _code;
 
         public Verify(String username, String email, String password, Login login, String connectionString)
         {
@@ -28,6 +32,15 @@ namespace FinalProject
             _login = login;
             _connectionString = connectionString;
             InitializeComponent();
+            generateCode();
+            sendCode();
+        }
+
+        // Generates the random integer to be sent to the user
+        public static void generateCode()
+        {
+            Random rand = new Random();
+            _code = Convert.ToString(rand.Next(100000, 1000000));
         }
 
         private void verCodeTextBox_TextChanged(object sender, EventArgs e)
@@ -35,6 +48,40 @@ namespace FinalProject
 
         }
 
+        // Sends the verification code to the user's registered email
+        private void sendCode()
+        {
+            MailMessage msg = new MailMessage();
+
+            msg.From = new MailAddress("therealcardsystem@gmail.com");
+            msg.To.Add(_email);
+            msg.Subject = "Verification Code";
+            msg.Body = "Verification Code: " + _code;
+            SmtpClient client = new SmtpClient();
+            client.UseDefaultCredentials = true;
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Credentials = new NetworkCredential("therealcardsystem@gmail.com", "18241918");
+            client.Timeout = 20000;
+            try
+            {
+                client.Send(msg);
+                Console.WriteLine("Mail has been successfully sent!");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Fail Has error" + ex.Message);
+            }
+            finally
+            {
+                msg.Dispose();
+            }
+        }
+
+        // if the user has inputted the correct verification code, CARD is opened with the user's information
         private void enterButton_Click(object sender, EventArgs e)
         {
             bool isValidCode = verCodeTextBox.Text.Equals(_code);
